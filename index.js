988f39af98e9b6a895bcfa3c56513ae8a7ba8589
lrsleandro <lrsleandro@gmail.com>
2020-03-04 17:08:54 -0300

    Atualização ConvertMyMoney

--- index.js
+++ index.js
@@ -2,30 +2,32 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const port = process.env.PORT || 4200
-
 const convert = require("./lib/convert")
 const apiBCB = require('./lib/api.bcb')
+
+const port = process.env.PORT || 4200
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', async(req, res) => {
-    cotacao = await apiBCB.getCotacao()
-    res.render('home', { cotacao })
+app.get('/', async(req,res) => {
+    const cotacao = await apiBCB.getCotacao()
+    res.render('home', {
+        cotacao
+    })
 })
-app.get('/cotacao', (req, res) => {
+app.get('/cotacao', (req,res) => {
     const { cotacao, quantidade } = req.query
     if(cotacao && quantidade){
-        conversao = convert.convert(cotacao, quantidade)
-        res.render('cotacao', { 
+        const conversao = convert.convert(cotacao, quantidade)
+        res.render('cotacao', {
             error: false,
             cotacao: convert.toMoney(cotacao),
-            quantidade: convert.toMoney(quantidade),
+            quantidade: convert.toMoney(quantidade) ,
             conversao: convert.toMoney(conversao)
         })
-    } else {
+    }else{
         res.render('cotacao', {
             error: 'Valores inválidos'
         })
--- teste-api.js
+++ teste-api.js
@@ -1,10 +1,9 @@
 const axios = require('axios')
 
-// mm-dd-aaaa
-const  data  =  '03-02-2020'
-const  url  =  `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${data}'&$top=100&$format=json========`
+// dataCotacao=%27 data 03-03-2020 mm-dd-aaaa
+const url = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2703-03-2020%27&$top=100&$skip=0&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao======`
 
-// URL correta | https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${data}'&$top=100&$format=json
+// URL correta | https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2703-03-2020%27&$top=100&$skip=0&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao
 
 axios
     .get(url)
